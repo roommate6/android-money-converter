c9019bf652916b9b2911b4a23e0d392357a4f5d5
roommate6 <floginstefan@gmail.com>
2024-02-23 05:02:05 +0200

    now we can convert from all curencies, not just from euros. Added string resource variables as well

--- app/src/main/java/com/example/laboratory_1_money_converter/MainActivity.java
+++ app/src/main/java/com/example/laboratory_1_money_converter/MainActivity.java
@@ -3,16 +3,14 @@ package com.example.laboratory_1_money_converter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private EditText _euroEditText, _dollarEditText, _ronEditText;
-    private Button _clearButton, _convertButton;
 
     private final double _dollarRate = 1.234;
-    private final double _ronRate = 5.897;
+    private final double _ronRate = 5.1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,29 +22,63 @@ public class MainActivity extends AppCompatActivity {
         _dollarEditText = findViewById(R.id.dollarEditText);
         _ronEditText = findViewById(R.id.ronEditText);
 
-        _convertButton = findViewById(R.id.convertButton);
-        _clearButton = findViewById(R.id.clearButton);
+        Button _convertButton = findViewById(R.id.convertButton);
+        Button _clearButton = findViewById(R.id.clearButton);
 
         // deal with events from buttons
-        _clearButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view){
-                _euroEditText.setText("");
-                _dollarEditText.setText("");
-                _ronEditText.setText("");
-            }
-        });
+        _clearButton.setOnClickListener(view -> handleClearButtonClickEvent());
 
-        _convertButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view){
-                double euros = Double.parseDouble(_euroEditText.getText().toString());
-                double dollars = euros*_dollarRate;
-                double rons = euros*_ronRate;
+        _convertButton.setOnClickListener(view -> handleConvertButtonClickEvent());
+    }
 
-                _dollarEditText.setText(String.valueOf(dollars));
-                _ronEditText.setText(String.valueOf(rons));
-            }
-        });
+    private void clear(){
+        _euroEditText.setText("");
+        _dollarEditText.setText("");
+        _ronEditText.setText("");
+    }
+
+    private void handleClearButtonClickEvent() {
+        clear();
+    }
+    private void handleConvertButtonClickEvent() {
+        String euroRowValue = _euroEditText.getText().toString();
+        String dollarRowValue = _dollarEditText.getText().toString();
+        String ronRowValue = _ronEditText.getText().toString();
+
+        if (!euroRowValue.equals("")){
+            convertFromEuro(Double.parseDouble(euroRowValue));
+            return;
+        }
+
+        if (!dollarRowValue.equals("")){
+            convertFromDollar(Double.parseDouble(dollarRowValue));
+            return;
+        }
+
+        convertFromRon(Double.parseDouble(ronRowValue));
+    }
+
+    private void convertFromEuro(double euros){
+        double dollars = euros*_dollarRate;
+        double rons = euros*_ronRate;
+
+        _dollarEditText.setText(String.valueOf(dollars));
+        _ronEditText.setText(String.valueOf(rons));
+    }
+
+    private void convertFromDollar(double dollars){
+        double euros = dollars/_dollarRate;
+        double rons = euros*_ronRate;
+
+        _euroEditText.setText(String.valueOf(euros));
+        _ronEditText.setText(String.valueOf(rons));
+    }
+
+    private void convertFromRon(double rons){
+        double euros = rons/_ronRate;
+        double dollars = euros*_dollarRate;
+
+        _euroEditText.setText(String.valueOf(euros));
+        _dollarEditText.setText(String.valueOf(dollars));
     }
 }
